ac8fd3977d610ac6f82e95be34e6fc54da8e1340
Aung Myo Kyaw <amk.maeu@gmail.com>
2017-07-04 20:41:21 +0630

    add support for browser

--- main.js
+++ main.js
@@ -3,6 +3,15 @@ var fontConvert = require('./library/converter');
 var syllBreak = require('./library/syllBreak');
 var spellingFix = require('./library/spellingCheck')
 
+if (typeof window !== 'undefined') {
+	window.knayi = {
+		fontDetect,
+		fontConvert,
+		syllBreak,
+		spellingFix
+	}
+}
+
 module.exports = {
 	fontDetect,
 	fontConvert,
