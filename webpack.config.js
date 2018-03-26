1bdc3a90dc2e24206535a56e9362030f12642807
Thuya Myo Nyunt <ty@kny.co>
2018-03-26 14:49:00 +0630

    Compile for commonjs2

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,26 @@
-var webpack = require('webpack');
+const webpack = require('webpack');
 
-module.exports = {
+const esConfig = {
+  entry: {
+    "knayi-myscript": __dirname + "/main.js"
+  },
+  output: {
+    path: __dirname + "/dist/",
+    filename: "[name].es.js",
+    library: "knayi",
+    libraryTarget: "commonjs2"
+  },
+  module: {
+    rules: [{
+      test: /\.js$/, 
+      exclude: /(node_modules)/, 
+      loader: 'babel-loader', 
+      query: {presets: ['env'] } 
+    }]
+  }
+}
+
+const legacyConfig = {
   entry: {
     "knayi-myscript": __dirname + "/main.js",
     "knayi-myscript.min": __dirname + "/main.js",
@@ -9,25 +29,20 @@ module.exports = {
   output: {
     path: __dirname + "/dist/",
     filename: "[name].js",
-	  library:"knayi",
-	  libraryTarget:"window"
+    library: "knayi",
+    libraryTarget: "window"
   },
   module: {
-  rules: [
-    {
-      test: /\.js$/,
-      exclude: /(node_modules)/,
-      loader: 'babel-loader',
-      query: {
-        presets: ['env']
-      }
-    }
-  ]
-},
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      include: /\.min\.js$/,
-      minimize: true
-    })
-  ]
+    rules: [{
+      test: /\.js$/, 
+      exclude: /(node_modules)/, 
+      loader: 'babel-loader', 
+      query: {presets: ['env'] } 
+    }]
+  }
 }
+
+
+module.exports = [
+  esConfig, legacyConfig
+]
