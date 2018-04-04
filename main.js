10eba57cefe222339a5076a06724283baf36356b
Thuya Myo Nyunt <ty@kny.co>
2018-04-05 01:19:31 +0630

    Bugfix converting u100bu1039u100c <-> u1092, fix regexp order in unicode to zawgyi and add version in knayi.version

--- main.js
+++ main.js
@@ -3,11 +3,14 @@ const fontConvert = require('./library/converter')
 const syllBreak = require('./library/syllBreak')
 const spellingFix = require('./library/spellingCheck')
 
+const {version} = require('./package.json')
+
 const setGlobalOptions = (options = {}) => {
 	fontDetect.__setOptions(options.detector)
 }
 
 module.exports = {
+	version,
 	setGlobalOptions,
 	fontDetect,
 	fontConvert,
